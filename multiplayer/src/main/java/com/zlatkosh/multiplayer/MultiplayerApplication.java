2a3c876ddbeb4a25379b77ccd56158a2494dbbaa
Zlatko Sharkoski <zlatko.sharkoski@clarivate.com>
2022-09-20 21:46:07 +0200

    #1 create initial project structure

---
+++ multiplayer/src/main/java/com/zlatkosh/multiplayer/MultiplayerApplication.java
@@ -0,0 +1,13 @@
+package com.zlatkosh.multiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplayerApplication.class, args);
+	}
+
+}
---
+++ multiplayer/src/test/java/com/zlatkosh/multiplayer/MultiplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlatkosh.multiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
