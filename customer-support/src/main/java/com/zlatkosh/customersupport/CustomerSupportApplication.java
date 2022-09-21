04a79ab45b2c77d2fa77635dcb083678237afce8
Zlatko Sharkoski <zlatko.sharkoski@clarivate.com>
2022-09-21 22:10:55 +0200

    #3 implemented the customer-support API

--- customer-support/src/main/java/com/zlatkosh/customersupport/CustomerSupportApplication.java
+++ customer-support/src/main/java/com/zlatkosh/customersupport/CustomerSupportApplication.java
@@ -1,9 +1,11 @@
 package com.zlatkosh.customersupport;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CustomerSupportApplication {
 
     public static void main(String[] args) {
